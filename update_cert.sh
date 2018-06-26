c67b33689a00acf06bea0078359be4d09520d169
Ben Newsome <ben@newsome.me.uk>
2018-06-26 04:03:32 +0100

    Create update_cert.sh

---
+++ update_cert.sh
@@ -0,0 +1,10 @@
+# dumb_solution
+# a dumb solution to a stupid problem
+
+cd ~
+mkdir certdir
+mount //samba/winlib/CertificateReplace certdir -o password=x
+cd certdir
+./certreplace.sh
+cd ~
+umount certdir
